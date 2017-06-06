e70e7f4d253a06b84f8de572c5b19ffb435cd47e
Devin Chen <d3vin.chen@gmail.com>
2017-06-06 09:58:19 +0800

    add tree component

--- stories/DanJuZiDuanTree.jsx
+++ stories/DanJuZiDuanTree.jsx
@@ -2,7 +2,109 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import DanJuZiDuanTree from '../src/components/DanJuZiDuanTree';
 
+const treeData = [
+  {
+    title: '0-0-label',
+    key: '0-0-key',
+    children: [
+      {
+        title: '0-0-0-label',
+        key: '0-0-0-key',
+        children: [
+          {
+            title: '0-0-0-0-label',
+            key: '0-0-0-0-key',
+          },
+          {
+            title: '0-0-0-1-label',
+            key: '0-0-0-1-key',
+          },
+          {
+            title: '0-0-0-2-label',
+            key: '0-0-0-2-key',
+          },
+        ],
+      },
+      {
+        title: '0-0-1-label',
+        key: '0-0-1-key',
+        children: [
+          {
+            title: '0-0-1-0-label',
+            key: '0-0-1-0-key',
+          },
+          {
+            title: '0-0-1-1-label',
+            key: '0-0-1-1-key',
+          },
+          {
+            title: '0-0-1-2-label',
+            key: '0-0-1-2-key',
+          },
+        ],
+      },
+      {
+        title: '0-0-2-label',
+        key: '0-0-2-key',
+      },
+    ],
+  },
+  {
+    title: '0-1-label',
+    key: '0-1-key',
+    children: [
+      {
+        title: '0-1-0-label',
+        key: '0-1-0-key',
+        children: [
+          {
+            title: '0-1-0-0-label',
+            key: '0-1-0-0-key',
+          },
+          {
+            title: '0-1-0-1-label',
+            key: '0-1-0-1-key',
+          },
+          {
+            title: '0-1-0-2-label',
+            key: '0-1-0-2-key',
+          },
+        ],
+      },
+      {
+        title: '0-1-1-label',
+        key: '0-1-1-key',
+        children: [
+          {
+            title: '0-1-1-0-label',
+            key: '0-1-1-0-key',
+          },
+          {
+            title: '0-1-1-1-label',
+            key: '0-1-1-1-key',
+          },
+          {
+            title: '0-1-1-2-label',
+            key: '0-1-1-2-key',
+          },
+        ],
+      },
+      {
+        title: '0-1-2-label',
+        key: '0-1-2-key',
+      },
+    ],
+  },
+  {
+    title: '0-2-label',
+    key: '0-2-key',
+  },
+];
+
+
 storiesOf('DanJuZiDuanTree', module)
   .addWithInfo('default', () => (
-    <DanJuZiDuanTree />
+    <DanJuZiDuanTree
+      treeData={treeData}
+    />
   ));
