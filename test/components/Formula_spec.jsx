a3f4070f7649c0942ab26b92b99ef0bccdb2aa06
Devin Chen <d3vin.chen@gmail.com>
2017-06-07 13:19:17 +0800

    add fake test for component

---
+++ test/components/Formula_spec.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+import { expect } from 'chai';
+// import { shallow } from 'enzyme';
+
+// import Formula from '../../src/components/Formula';
+
+describe('Formula', () => {
+  describe('#render', () => {
+    it('is .Formula class', () => {
+      // const wrapper = shallow(<Formula />);
+      // expect(wrapper.is('.Formula')).to.equal(true);
+      expect(true).to.equal(true);
+    });
+  });
+});
