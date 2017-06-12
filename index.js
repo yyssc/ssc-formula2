803cc4fa251a971184a7da97394e078dfa32a8a8
Devin Chen <d3vin.chen@gmail.com>
2017-06-12 22:08:11 +0800

    fix #7 es6 shorthand property causes uglify-js crash

--- index.js
+++ index.js
@@ -1,56 +1,11 @@
-// var DateRangePicker = require('./lib/components/DateRangePicker').default;
-// var DateRangePickerInput = require('./lib/components/DateRangePickerInput').default;
-// var DateRangePickerInputController =
-//   require('./lib/components/DateRangePickerInputController').default;
-// var SingleDatePicker = require('./lib/components/SingleDatePicker').default;
-// var SingleDatePickerInput = require('./lib/components/SingleDatePickerInput').default;
-// var DayPicker = require('./lib/components/DayPicker').default;
-// var DayPickerRangeController = require('./lib/components/DayPickerRangeController').default;
-// var CalendarMonthGrid = require('./lib/components/CalendarMonthGrid').default;
-// var CalendarMonth = require('./lib/components/CalendarMonth').default;
-// var CalendarDay = require('./lib/components/CalendarDay').default;
 const Formula = require('./lib/components/Formula').default;
 
-// var DateRangePickerShape = require('./lib/shapes/DateRangePickerShape').default;
-// var SingleDatePickerShape = require('./lib/shapes/SingleDatePickerShape').default;
 const treeDataShape = require('./lib/shapes/treeDataShape').default;
 const 档案转换ItemShape = require('./lib/shapes/档案转换ItemsShape').default;
 
-// var isInclusivelyAfterDay = require('./lib/utils/isInclusivelyAfterDay').default;
-// var isInclusivelyBeforeDay = require('./lib/utils/isInclusivelyBeforeDay').default;
-// var isNextDay = require('./lib/utils/isNextDay').default;
-// var isSameDay = require('./lib/utils/isSameDay').default;
-
-// var toISODateString = require('./lib/utils/toISODateString').default;
-// var toLocalizedDateString = require('./lib/utils/toLocalizedDateString').default;
-// var toMomentObject = require('./lib/utils/toMomentObject').default;
-
-
 module.exports = {
-  // DateRangePicker: DateRangePicker,
-  // SingleDatePicker: SingleDatePicker,
+  Formula: Formula,
 
-  // DateRangePickerInputController: DateRangePickerInputController,
-  // DateRangePickerInput: DateRangePickerInput,
-  // SingleDatePickerInput: SingleDatePickerInput,
-  // DayPicker: DayPicker,
-  // DayPickerRangeController: DayPickerRangeController,
-  // CalendarMonthGrid: CalendarMonthGrid,
-  // CalendarMonth: CalendarMonth,
-  // CalendarDay: CalendarDay,
-  Formula,
-
-  // DateRangePickerShape: DateRangePickerShape,
-  // SingleDatePickerShape: SingleDatePickerShape,
-  treeDataShape,
-  档案转换ItemShape,
-
-  // isInclusivelyAfterDay: isInclusivelyAfterDay,
-  // isInclusivelyBeforeDay: isInclusivelyBeforeDay,
-  // isNextDay: isNextDay,
-  // isSameDay: isSameDay,
-
-  // toISODateString: toISODateString,
-  // toLocalizedDateString: toLocalizedDateString,
-  // toMomentObject: toMomentObject,
+  treeDataShape: treeDataShape,
+  档案转换ItemShape: 档案转换ItemShape,
 };
