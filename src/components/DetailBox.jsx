96be85d89fd74c039982f5e86de557c7edb8452f
Devin Chen <d3vin.chen@gmail.com>
2017-07-11 15:39:54 +0800

    unittest for DetailBox

--- src/components/DetailBox.jsx
+++ src/components/DetailBox.jsx
@@ -52,7 +52,7 @@ export default class DetailBox extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="detail-box">
         <hr />
         {
           this.props.details.map(detail => (
---
+++ test/components/DetailBox_spec.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import DetailBox from '../../src/components/DetailBox';
+
+describe('Formula', () => {
+  describe('#render', () => {
+    it('is .Formula class', () => {
+      const wrapper = shallow(
+        <DetailBox
+          details={['some text...']}
+        />,
+      );
+      expect(wrapper.is('.detail-box')).to.equal(true);
+    });
+  });
+});
